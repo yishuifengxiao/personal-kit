6180c4be0b8f9e3476eb88c330aab956d70ee49f
zhiyubujian <zhiyubujian@gmail.com>
2024-11-11 11:00:41 +0800

    init

--- personal-kit/src/main/java/com/yishuifengxiao/tool/personalkit/security/SimpleCustomResourceConfigurator.java
+++ personal-kit/src/main/java/com/yishuifengxiao/tool/personalkit/security/SimpleCustomResourceConfigurator.java
@@ -14,17 +14,10 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
-import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.core.annotation.Order;
-import org.springframework.expression.EvaluationContext;
-import org.springframework.expression.ExpressionParser;
-import org.springframework.expression.spel.standard.SpelExpressionParser;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.web.access.expression.DefaultHttpSecurityExpressionHandler;
-import org.springframework.security.web.access.expression.WebSecurityExpressionRoot;
 import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.AnyRequestMatcher;
@@ -32,7 +25,6 @@ import org.springframework.security.web.util.matcher.OrRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
@@ -65,16 +57,6 @@ public class SimpleCustomResourceConfigurator implements CustomResourceConfigura
         HttpServletRequest request = object.getRequest();
 
         Authentication authentication = supplier.get();
-//        if (null != authentication && AnonymousAuthenticationToken.class.isAssignableFrom
-//        (authentication.getClass())) {
-//            return new AuthorizationDecision(true);
-//        }
-
-
-        AuthorizationDecision decision = authorizationDecision(supplier, object);
-        if (null != decision) {
-            return decision;
-        }
 
         SysUser sysUser =
                 sysUserDao.findActiveSysUser(authentication.getName()).orElseThrow(() -> new UsernameNotFoundException(String.format("账号%s不存在", authentication.getName())));
@@ -104,54 +86,6 @@ public class SimpleCustomResourceConfigurator implements CustomResourceConfigura
         boolean anyMatch = orRequestMatcher.matches(request);
 
         return new AuthorizationDecision(anyMatch);
-    }
-
-
-    private AuthorizationDecision authorizationDecision(Supplier<Authentication> supplier,
-                                                        RequestAuthorizationContext object) {
-        Authentication authentication = supplier.get();
-
-        DefaultHttpSecurityExpressionHandler defaultHttpSecurityExpressionHandler =
-                new DefaultHttpSecurityExpressionHandler();
-        WebSecurityExpressionRoot webSecurityExpressionRoot =
-                new WebSecurityExpressionRoot(supplier,
-                object.getRequest());
-        EvaluationContext evaluationContext =
-                defaultHttpSecurityExpressionHandler.createEvaluationContext(supplier,
-                object);
-        // 使用 SpEL 解析表达式
-        ExpressionParser parser = new SpelExpressionParser();
-
-
-        HttpServletRequest request = object.getRequest();
-        String uri = request.getRequestURI();
-        uri = StringUtils.substringAfter(uri, this.contextPath);
-        SysPermission permission =
-                JdbcUtil.jdbcHelper(context).findOne(new SysPermission().setUrl(uri),
-                false);
-        if (null == permission) {
-            return null;
-        }
-        try {
-
-            String[] tokens =
-                    StringUtils.splitByWholeSeparatorPreserveAllTokens(permission.getPath(), "::");
-            String preAuthorize =
-                    Arrays.stream(Class.forName(tokens[0]).getMethods()).filter(v -> StringUtils.equals(v.getName(),
-                            tokens[1])).findFirst().map(method -> AnnotationUtils.findAnnotation(method,
-                            PreAuthorize.class)).map(PreAuthorize::value).orElse(null);
-            if (StringUtils.isNotBlank(preAuthorize)) {
-
-                Boolean value = parser.parseExpression(preAuthorize).getValue(evaluationContext,
-                        Boolean.class);
-                if (null != value) {
-                    return new AuthorizationDecision(value);
-                }
-            }
-
-        } catch (Exception e) {
-        }
-        return null;
     }
 
     @Override
