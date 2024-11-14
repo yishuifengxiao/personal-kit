7cc35645c6bf08c47c9bb505ea505abf57f5424e
zhiyubujian <zhiyubujian@gmail.com>
2024-11-14 09:25:50 +0800

    init

--- personal-kit/src/main/java/com/yishuifengxiao/tool/personalkit/security/SimpleCustomResourceConfigurator.java
+++ personal-kit/src/main/java/com/yishuifengxiao/tool/personalkit/security/SimpleCustomResourceConfigurator.java
@@ -5,20 +5,16 @@ import com.yishuifengxiao.common.security.httpsecurity.authorize.custom.CustomRe
 import com.yishuifengxiao.common.tool.collections.CollUtil;
 import com.yishuifengxiao.common.tool.utils.ValidateUtils;
 import com.yishuifengxiao.tool.personalkit.dao.SysUserDao;
-import com.yishuifengxiao.tool.personalkit.domain.constant.Constant;
 import com.yishuifengxiao.tool.personalkit.domain.entity.SysRole;
 import com.yishuifengxiao.tool.personalkit.domain.entity.SysUser;
-import com.yishuifengxiao.tool.personalkit.domain.enums.UserStat;
 import com.yishuifengxiao.tool.personalkit.support.ContextCache;
 import jakarta.servlet.http.HttpServletRequest;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.AnyRequestMatcher;
@@ -60,13 +56,8 @@ public class SimpleCustomResourceConfigurator implements CustomResourceConfigura
         HttpServletRequest request = object.getRequest();
 
         Authentication authentication = supplier.get();
+        SysUser sysUser = ContextCache.currentUser(authentication).orElse(null);
 
-        SysUser sysUser =
-                sysUserDao.findActiveSysUser(authentication.getName()).orElseThrow(() -> new UsernameNotFoundException(String.format("账号%s不存在", authentication.getName())));
-        // 内置账号默认通过校验
-        if (StringUtils.equalsIgnoreCase(sysUser.getId(), Constant.DEFAULT_ROOT_ID) || UserStat.SYSTEM_INIT.equalCode(sysUser.getStat())) {
-            return new AuthorizationDecision(true);
-        }
         //当前角色
         String currentRole =
                 ContextCache.getRole().map(SysRole::getId).orElseThrow(ValidateUtils.orElseThrow(
@@ -122,7 +113,8 @@ public class SimpleCustomResourceConfigurator implements CustomResourceConfigura
             String sql = """
                     SELECT DISTINCT s.url from sys_permission s where s.application_name=?
                     """;
-            List<String> list = JdbcUtil.jdbcHelper(context).findAll(String.class, sql, applicationName);
+            List<String> list = JdbcUtil.jdbcHelper(context).findAll(String.class, sql,
+                    applicationName);
             if (CollUtil.isEmpty(list)) {
                 return null;
             }
